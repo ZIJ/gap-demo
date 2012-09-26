2911829ae670f5c275162264c95b431694747277
Igor Zalutski <igor.zalutsky@gmail.com>
2012-09-26 15:54:34 +0300

    Moved js libraries to www/js/lib folder; first accelerometer implementation

--- www/js/index.js
+++ www/js/index.js
@@ -58,9 +58,50 @@
             }
         });
 
+        var AccelerometerModel = Backbone.Model.extend({
+            defaults: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+            initialize: function(){
+                var that = this;
+                var options = { frequency: 30 };
+                try {
+                    navigator.accelerometer.watchAcceleration(function(acceleration){
+                        that.set({
+                            x: acceleration.x,
+                            y: acceleration.y,
+                            z: acceleration.z
+                        });
+                    }, options);
+                } catch (e) {
+                    console.log(e.message);
+                }
+            }
+        });
+
+        var AccelerometerView = Backbone.View.extend({
+            initialize: function(){
+                this.model.on('change', this.render, this);
+                this.render();
+                console.log("Accelerometer view created");
+            },
+            render: function(){
+                var source = $("#accelerometer-template").html();
+                var template = Handlebars.compile(source);
+                var json = this.model.toJSON();
+                var markup = template(json);
+                $(this.el).html(markup);
+            }
+        });
+
+
+
         var AppModel = Backbone.Model.extend({
             initialize: function(){
                 this.info = new InfoModel();
+                this.accelerometer = new AccelerometerModel();
                 console.log("App model created");
             }
         });
@@ -73,11 +114,13 @@
         var AppView = Backbone.View.extend({
             initialize: function(){
                 this.infoView = new InfoView({model: app.info, el: $("#info")});
+                this.accelerometerView = new AccelerometerView({model: app.accelerometer, el: $("#accelerometer")});
                 this.render();
                 console.log("App view created");
             },
             render: function(){
                 this.infoView.render();
+                this.accelerometerView.render();
             },
             page: function(pageName){
                 $(".page").hide();
